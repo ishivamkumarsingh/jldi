211a55ca6649b5e1530c6aa57fd4efb117237ff7
ishivamkumarsingh <140041564+ishivamkumarsingh@users.noreply.github.com>
2025-03-15 17:13:05 +0530

    Update TestRunner.java

--- nop/src/test/java/com/nopcommerece/runners/TestRunner.java
+++ nop/src/test/java/com/nopcommerece/runners/TestRunner.java
@@ -1,15 +1,18 @@
-package com.nopcommerece.runners;
+package com.nopcommerce.runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
     features = "src/test/resources/features",
-    glue = "com.nopcommerce.stepdefinitions",
-    plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json"},
+    glue = {"com.nopcommerce.stepdefinitions", "com.nopcommerce.util"},
+    plugin = {
+        "pretty",
+        "html:target/cucumber-reports/cucumber-report.html",
+        "json:target/cucumber-reports/cucumber-report.json",
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+    },
     monochrome = true
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
-
-
